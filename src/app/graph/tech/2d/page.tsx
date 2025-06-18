5d7cf298e2a6ed71fc5dfd2d790519575f2f1bba
isixline <isixline@qq.com>
2025-06-18 11:18:07 +0800

    refactor: add Graph2D component

--- src/app/graph/tech/2d/page.tsx
+++ src/app/graph/tech/2d/page.tsx
@@ -1,50 +1,10 @@
-"use client";
+'use client'
 
-import React, { useRef, useCallback, useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-
-import NodeDetails from "@/components/NodeDetails";
 import { getTechGraph } from "@/lib/api/graph";
+import Graph2D from "@/components/Graph2D";
 
-const ForceGraph2D = dynamic(() => import("react-force-graph-2d"), { ssr: false });
-
-export default function AutoLayoutForceGraph2D() {
-    const fgRef = useRef<any>(null);
-    const [graphData, setGraphData] = useState<{ nodes: any[]; links: any[] }>({
-        nodes: [],
-        links: [],
-    });
-    const [selectedNode, setSelectedNode] = useState<any>(null);
-
-    useEffect(() => {
-        getTechGraph()
-            .then((data) => {
-                setGraphData(data)
-            })
-    }, []);
-
-    const handleNodeClick = useCallback((node: any) => {
-        if (!fgRef.current || !node) return;
-
-        setSelectedNode(node);
-        // 平移视图使节点居中
-        fgRef.current.centerAt(node.x, node.y, 1000);
-        fgRef.current.zoom(4, 1000);
-    }, []);
-
+export default function Page() {
     return (
-        <div style={{ width: "100%", height: "600px", position: "relative" }}>
-            <ForceGraph2D
-                ref={fgRef}
-                graphData={graphData}
-                nodeAutoColorBy="group"
-                nodeLabel={(node: any) => `${node.name}`}
-                onNodeClick={handleNodeClick}
-                linkDirectionalParticles={2}
-                linkDirectionalParticleWidth={1}
-            />
-
-            <NodeDetails node={selectedNode} open={!!selectedNode} />
-        </div>
+        <Graph2D fetchData={getTechGraph} />
     );
 }
---
+++ src/components/Graph2D.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React, { useRef, useCallback, useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import NodeDetails from "@/components/NodeDetails";
+import { GraphData } from "@/types/graph";
+
+const ForceGraph2D = dynamic(() => import("react-force-graph-2d"), { ssr: false });
+
+interface Props {
+    fetchData: () => Promise<GraphData>;
+}
+
+export default function Graph2D({ fetchData }: Props) {
+    const fgRef = useRef<any>(null);
+    const [graphData, setGraphData] = useState<GraphData>({
+        nodes: [],
+        links: [],
+    });
+    const [selectedNode, setSelectedNode] = useState<any>(null);
+
+    useEffect(() => {
+        fetchData()
+            .then((data) => {
+                setGraphData(data)
+            })
+    }, [fetchData]);
+
+    const handleNodeClick = useCallback((node: any) => {
+        if (!fgRef.current || !node) return;
+
+        setSelectedNode(node);
+        // 平移视图使节点居中
+        fgRef.current.centerAt(node.x, node.y, 1000);
+        fgRef.current.zoom(4, 1000);
+    }, []);
+
+    return (
+        <div style={{ width: "100%", height: "600px", position: "relative" }}>
+            <ForceGraph2D
+                ref={fgRef}
+                graphData={graphData}
+                nodeAutoColorBy="group"
+                nodeLabel={(node: any) => `${node.name}`}
+                onNodeClick={handleNodeClick}
+                linkDirectionalParticles={2}
+                linkDirectionalParticleWidth={1}
+            />
+
+            <NodeDetails node={selectedNode} open={!!selectedNode} />
+        </div>
+    );
+}
